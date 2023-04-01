0c749e66c690adb62e82575a4724ebad6d2d8325
Sarthak Gupta <sg191107@gmail.com>
2023-04-01 14:45:31 +0530

    corrected Queries

--- src/main/java/com/emotunes/emotunes/repository/UserSongMappingRepository.java
+++ src/main/java/com/emotunes/emotunes/repository/UserSongMappingRepository.java
@@ -16,30 +16,27 @@ public interface UserSongMappingRepository
 
 
     @Query(value =
-            "select * from user_song_emotion_mapping"
+            "select * from user_song_emotion_mapping "
                     + " where user_id = ?1 and "
-                    + "  id < ?2 and "
-                    + " limit ?3",
+                    + " id < ?2 limit ?3",
             nativeQuery = true
     )
     List<StoredUserSongMapping> findNextPageOfSongsFromAllCategory(StoredUser storedUser, String lastFetchedId, int pageSize);
 
     @Query(value =
             "select * from user_song_emotion_mapping"
-                    + " where user_id = ?1"
+                    + " where user_id = ?1 and "
                     + " id < ?2 and "
-                    + " and emotion = ?3 "
-                    + " limit ?4",
+                    + " emotion = ?3 limit ?4",
             nativeQuery = true
     )
     List<StoredUserSongMapping> findNextPageOfSongsWithEmotionOfUser(StoredUser storedUser, String lastFetchedId, String emotion, int pageSize);
 
     @Query(value =
             "select * from user_song_emotion_mapping"
-                    + " where user_id = ?1"
+                    + " where user_id = ?1 and "
                     + " id < ?2 and "
-                    + " and is_liked = 1 "
-                    + " limit ?3",
+                    + " is_liked = 1 limit ?3",
             nativeQuery = true
     )
     List<StoredUserSongMapping> findNextPageOfLikedSongsOfUser(StoredUser storedUser, String lastFetchedId, int pageSize);
