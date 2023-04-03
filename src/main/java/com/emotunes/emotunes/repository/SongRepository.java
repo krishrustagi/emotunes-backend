bb1dc5951687f9418abc6da73cda8676f7cd2248
Krish Rustagi <54409969+krishrustagi@users.noreply.github.com>
2023-04-03 21:46:32 +0530

    optimize imports

--- src/main/java/com/emotunes/emotunes/repository/SongRepository.java
+++ src/main/java/com/emotunes/emotunes/repository/SongRepository.java
@@ -2,11 +2,7 @@ package com.emotunes.emotunes.repository;
 
 import com.emotunes.emotunes.entity.StoredSong;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalTime;
-import java.util.List;
 
 @Repository
 public interface SongRepository extends JpaRepository<StoredSong, String> {
