3a7df2a365f13a6fde8328fcc29e32e400d49a70
Krish Rustagi <54409969+krishrustagi@users.noreply.github.com>
2023-04-28 16:43:21 +0530

    test

--- emotunes-server/src/main/java/com/emotunes/emotunes/config/AzureBlobConfig.java
+++ emotunes-server/src/main/java/com/emotunes/emotunes/config/AzureBlobConfig.java
@@ -1,23 +1,60 @@
 package com.emotunes.emotunes.config;
 
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.common.policy.RequestRetryOptions;
+import com.azure.storage.common.policy.RetryPolicyType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @Configuration
 public class AzureBlobConfig {
 
-    @Value("${azure.storage.connection.string}")
-    private String connectionString;
+    @Value("${app.config.azure.client-id}")
+    private String clientId;
+
+    @Value("${app.config.azure.client-secret}")
+    private String clientSecret;
+
+    @Value("${app.config.azure.tenant-id}")
+    private String tenantId;
+
+    @Value("${app.config.azure.storage-id}")
+    private String storageId;
+
+    @Value("${app.config.azure.storage-endpoint}")
+    private String storageEndpoint;
+
 
     @Bean
-    public BlobServiceClient clobServiceClient() {
-
+    public BlobServiceClientBuilder blobServiceClientBuilder() {
         return new BlobServiceClientBuilder()
-                .connectionString(connectionString)
-                .buildClient();
+                .credential(getAzureClientCredentials())
+                .endpoint(storageEndpoint);
+    }
 
+    private ClientSecretCredential getAzureClientCredentials() {
+        return new ClientSecretCredentialBuilder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .tenantId(tenantId)
+                .build();
+    }
+
+    @Bean
+    public BlobServiceClient blobServiceClient(BlobServiceClientBuilder blobServiceClientBuilder) {
+        return blobServiceClientBuilder.retryOptions(
+                new RequestRetryOptions(
+                        RetryPolicyType.EXPONENTIAL,
+                        5,
+                        Duration.ofSeconds(300L),
+                        null,
+                        null,
+                        null)).buildClient();
     }
 }
