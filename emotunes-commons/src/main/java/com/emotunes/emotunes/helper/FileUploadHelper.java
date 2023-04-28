9e9a45704851f5a88e09d01be855a351545260b8
Krish Rustagi <54409969+krishrustagi@users.noreply.github.com>
2023-04-28 10:31:33 +0530

    test

--- emotunes-commons/src/main/java/com/emotunes/emotunes/helper/FileUploadHelper.java
+++ emotunes-commons/src/main/java/com/emotunes/emotunes/helper/FileUploadHelper.java
@@ -4,6 +4,7 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,20 @@ import org.springframework.stereotype.Component;
 import java.io.InputStream;
 
 @Component
+@RequiredArgsConstructor
 public class FileUploadHelper {
 
     @Value("${azure.storage.connection.string}")
     private String connectionString;
 
+    private final BlobServiceClient blobServiceClient;
+
     public String uploadAndGetUrl(String containerName, InputStream inputStream, String fileName,
                                          long fileSize) {
-        BlobServiceClient blobServiceClient =
-                new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
+
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
         BlobClient blobClient = containerClient.getBlobClient(fileName);
-        blobClient.upload(inputStream, fileSize);
+        blobClient.upload(inputStream, fileSize, true);
 
         return blobClient.getBlobUrl();
     }
---
+++ emotunes-server/src/main/java/com/emotunes/emotunes/config/AzureBlobConfig.java
@@ -0,0 +1,26 @@
+package com.emotunes.emotunes.config;
+
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AzureBlobConfig {
+
+    @Value("${azure.storage.connection.string}")
+    private String connectionString;
+
+    @Value("${azure.storage.container.name}")
+    private String containerName;
+
+    @Bean
+    public BlobServiceClient clobServiceClient() {
+
+        return new BlobServiceClientBuilder()
+                .connectionString(connectionString)
+                .buildClient();
+
+    }
+}
