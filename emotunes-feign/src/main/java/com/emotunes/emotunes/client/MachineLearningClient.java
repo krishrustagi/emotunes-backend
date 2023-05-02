162fc513db5eb045fceb8ace6f965eb8d40ee959
Krish Rustagi <krishrustagi1@gmail.com>
2023-05-02 23:13:34 +0530

    Flask deployment changes (#63)

--- emotunes-feign/src/main/java/com/emotunes/emotunes/client/MachineLearningClient.java
+++ emotunes-feign/src/main/java/com/emotunes/emotunes/client/MachineLearningClient.java
@@ -2,10 +2,11 @@ package com.emotunes.emotunes.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "machineLearningClient", url = "http://localhost:5000")
+@FeignClient(name = "machineLearningClient", url = "https://emtounes.onrender.com/")
 public interface MachineLearningClient {
 
     @PostMapping(value = "/v1/predict")
